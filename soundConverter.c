fb95442e79949bde21ad89ef97a216e67eec1268
eaglewyng <parker.ridd@byu.net>
2014-10-23 23:37:20 -0600

    Initial source commit

---
+++ soundConverter.c
@@ -0,0 +1,10 @@
+/*
+ * soundConverter.c
+ *
+ *  Created on: Oct 23, 2014
+ *      Author: Parker Ridd & Tyler Bellows
+ */
+
+int main(int argc, char *argv[]){
+
+}
---
+++ soundConverter.h
@@ -0,0 +1,13 @@
+/*
+ * soundConverter.h
+ *
+ *  Created on: Oct 23, 2014
+ *      Author: Parker Ridd & Tyler Bellows
+ */
+
+#ifndef SOUNDCONVERTER_H_
+#define SOUNDCONVERTER_H_
+
+
+
+#endif /* SOUNDCONVERTER_H_ */
